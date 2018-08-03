cbb9d3f80596fd364a72d07ba6983b4c7eaef8fd
PARK Jongeui <jongeui.park@kaist.ac.kr>
2018-08-04 00:15:43 +0900

    set font

--- init.el
+++ init.el
@@ -3,13 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 108 :width normal)))))
 
 (setq straight-recipes-gnu-elpa-use-mirror t)
 
