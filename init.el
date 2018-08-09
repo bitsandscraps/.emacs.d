33aec2dfe73149f9b82573f2059923040376efcc
bitsandscraps <daniel9607@hanmail.net>
2018-08-10 07:09:11 +0900

    updated straight bootstrap

--- init.el
+++ init.el
@@ -53,6 +53,15 @@ Inserted by installing org-mode or when a release is made."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Okular")
+     (output-html "xdg-open"))))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
