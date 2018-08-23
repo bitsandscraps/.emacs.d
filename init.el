a279f212fde86866bbfe9613ab90804f4064100c
PARK Jongeui <jongeui.park@kaist.ac.kr>
2018-08-23 17:18:16 +0900

    Fix snippet directories

--- init.el
+++ init.el
@@ -3,7 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks)
+      "dvips and start")
+     (output-dvi "Yap")
+     (output-pdf "SumatraPDF")
+     (output-html "start"))))
  '(inhibit-startup-screen t)
+ '(preview-gs-command "mgs")
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
